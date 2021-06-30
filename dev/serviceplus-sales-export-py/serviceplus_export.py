24967eb16ea650367afdf61b40c4ecbeb2016b95
Sushant Agrawal <capitalch@gmail.com>
2021-06-30 10:48:08 +0530

    init

---
+++ dev/serviceplus-sales-export-py/serviceplus_export.py
@@ -0,0 +1,19 @@
+from tkinter import Tk
+from tkinter.constants import BOTH, X
+
+# from controls_frame import get_controls_frame, fetch_sale_data
+# from data_table_treeview import get_data_table_treeview
+
+root = Tk()
+root.title('Export serviceplus sales to Trace')
+root.geometry('800x600+200+200')
+
+# tv = get_data_table_treeview(root)
+
+# frame = get_controls_frame(root)
+# frame.pack(fill=X)
+
+# tv.pack(fill=BOTH, expand=1)
+# fetch_sale_data()
+
+root.mainloop()
