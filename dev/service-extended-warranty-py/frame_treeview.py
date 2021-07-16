45f092bfc43f7ac9ed01bc4b0741c17621435166
Sushant Agrawal <capitalch@gmail.com>
2021-07-16 21:16:43 +0530

    init

--- dev/service-extended-warranty-py/frame_treeview.py
+++ dev/service-extended-warranty-py/frame_treeview.py
@@ -1,8 +1,10 @@
-from tkinter.constants import BOTH, BOTTOM, CENTER, HORIZONTAL, RIGHT, X, Y
+from tkinter.constants import ANCHOR, BOTH, BOTTOM, CENTER, E, HORIZONTAL, LEFT, RIGHT, W, X, Y
 from tkinter.ttk import Treeview, Scrollbar
 from tkinter import Frame, messagebox
+from datetime import datetime
+from rx.core.observable.observable import C
 from utils import config
-import simplejson  as json
+import simplejson as json
 import requests
 from messages import messages
 from ibuki import Ibuki
@@ -12,15 +14,39 @@ class TreeviewFrame(Frame):
     def __init__(self, parent):
         Ibuki.filterOn('VIEW-EXTENDED-WARRANTY-CUSTOMERS').subscribe(lambda d:
                                                                      self.get_extended_warranty_customers())
-        columns = ('id', 'date')
+        columns = ('id', 'purchDate',  'daysLeft','custName', 'mobileNo', 'smsSentDates', 'serialNumber',
+                   'productCategory', 'address', 'pin',  'modelCode', 'modelName',)
         super().__init__(parent, highlightcolor='black',
                          highlightthickness=2, padx=10, pady=10)
-        tv = Treeview(parent, columns=columns, show='headings')
+        self.tv = Treeview(parent, columns=columns, show='headings')
+        tv = self.tv
         tv.column('id', width=50, minwidth=50, anchor=CENTER)
-        tv.column('date', width=50, minwidth=50, anchor=CENTER)
+        tv.column('purchDate', width=80, minwidth=50, anchor=CENTER)
+        tv.column('daysLeft', width=80, anchor=E)
+        tv.column('custName', width=150, anchor=W)
+        tv.column('mobileNo', width=80, anchor=W)
+        
+        tv.column('smsSentDates', width=100, anchor=W)
+        tv.column('serialNumber', width=80, anchor=W)
+        tv.column('productCategory', width=80, anchor=W)
+        tv.column('address', width=120, anchor=W)
+        tv.column('pin', width=60, anchor=CENTER)
+        tv.column('modelCode', width=100, anchor=W)
+        tv.column('modelName', width=100, anchor=W)
 
-        tv.heading('id', text='Sl')
-        tv.heading('date', text='Date')
+        tv.heading('id', text='Id')
+        tv.heading('purchDate', text='Purch date')
+        tv.heading('daysLeft', text='Days left', anchor=E)
+        tv.heading('custName', text='Cust name', anchor=W)
+        tv.heading('mobileNo', text='Mobile no', anchor=E)
+        
+        tv.heading('smsSentDates', text='SMS sent dates', anchor=W)
+        tv.heading('serialNumber', text='Serial no', anchor=W)
+        tv.heading('productCategory', text='Product cat', anchor=W)
+        tv.heading('address', text='Address', anchor=W)
+        tv.heading('pin', text='Pin', anchor=CENTER)
+        tv.heading('modelCode', text='Model code', anchor=W)
+        tv.heading('modelName', text='Model name', anchor=W)
 
         hsb = Scrollbar(parent, orient=HORIZONTAL)
         hsb.configure(command=tv.xview)
@@ -40,16 +66,38 @@ class TreeviewFrame(Frame):
     def get_extended_warranty_customers(self):
         try:
             result = requests.post(config.extendedWarrantyCustomersEndPoint)
-            jsn = json.loads(result.text)
-            print(jsn)
+            data = json.loads(result.text)
+            self.tv.delete(*self.tv.get_children())
+
+            def add_row(row):
+                self.tv.insert('', 'end', values=row)
+
+            show_data = [add_row(
+                (
+                    item['id'],
+                    datetime.strptime(item['purchDate'], '%Y-%m-%d').strftime('%d/%m/%Y'),
+                    int(item['daysLeft']),
+                    item['custName'],
+                    item['mobileNo'],                    
+                    item['smsSentDates'],
+                    item['serialNumber'],
+                    item['productCategory'],
+                    item['address'],
+                    int(float(item['pin'])),
+                    item['modelCode'],
+                    item['modelName']
+                )) for item in data]
+            # for item in show_data:
+            #     self.tv.insert('', 'end', values=item)
+            # print(data)
         except(Exception) as error:
-            print(error)
-        
+            messagebox.showerror('Error', error.args[0] or repr(
+                error) or messages.get('errGettingExtendedWarrantyCustomers'))
 
 
 def init_treeview_frame(root):
     try:
-        frame_treeview = TreeviewFrame(root)
+        frame_treeview = TreeviewFrame(root).tv
         frame_treeview.pack(fill=BOTH, padx=10, pady=10)
     except(Exception) as error:
         messagebox.showerror('Error', error or messages.get('errGeneric'))
--- dev/service-extended-warranty-py/messages.py
+++ dev/service-extended-warranty-py/messages.py
@@ -1,6 +1,7 @@
 messages = {
     'errGeneric': 'Operation could not be completed',
     'errSelectingFile': 'There was error in selection of file',
+    'errGettingExtendedWarrantyCustomers': "Error in getting extended warranty customers",
 
     'infoUploadSuccess':'Data successfully uploaded to server'
 }
