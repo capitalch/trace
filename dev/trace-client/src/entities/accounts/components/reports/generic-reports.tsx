ae2b683ff1437e36db1e0f7ab3dc57ea9d4473c3
Sushant Agrawal <capitalch@gmail.com>
2021-07-30 21:42:22 +0530

    init

--- dev/trace-client/src/entities/accounts/components/reports/generic-reports.tsx
+++ dev/trace-client/src/entities/accounts/components/reports/generic-reports.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from 'react'
 // import MaterialTable from 'material-table'
-import { XGrid, GridToolbar } from '@material-ui/x-grid'
+import { XGrid, GridToolbar, GridToolbarFilterButton, GridToolbarDensitySelector, GridToolbarExport, GridToolbarContainer, GridToolbarColumnsButton, GridFooter, GridFooterContainer } from '@material-ui/x-grid'
 // import { DataGrid, useGridApiRef, GridToolbar } from '@material-ui/data-grid'
 import { Theme, createStyles, makeStyles, Card, Typography, Box } from '@material-ui/core'
 import { manageEntitiesState } from '../../../../common-utils/esm'
@@ -9,6 +9,7 @@ import { useTraceGlobal } from '../../../../common-utils/trace-global'
 // import { tableIcons } from '../common/material-table-icons'
 import { useSharedElements } from '../common/shared-elements-hook'
 import { useAllTransactions } from './helpers/all-transactions'
+import { useGridSlotComponentProps } from '@material-ui/data-grid'
 
 function GenericReports({ loadReport }: any) {
     const selectLogic: any = {
@@ -16,6 +17,7 @@ function GenericReports({ loadReport }: any) {
     }
     const { args, columns, sqlQueryId, title } = selectLogic[loadReport]()
     const [rows, setRows]: any[] = useState([])
+    const [selectedRowsCount, setSelectedRowsCount] = useState(0)
     const [isMounted, setIsMounted] = useState(false)
 
     // const { getCurrentWindowSize } = useTraceGlobal()
@@ -51,25 +53,59 @@ function GenericReports({ loadReport }: any) {
                 columns={columns}
                 rows={rows}
                 rowHeight={32}
+
                 components={{
-                    Toolbar: CustomToolbar,
+                    Toolbar: CustomGridToolbar,
+                    Footer: CustomGridFooter,
                 }}
                 checkboxSelection={true}
+                componentsProps={{
+                    footer: { count: rows.length, selectedRowsCount: selectedRowsCount },
+                }}
+                onSelectionModelChange={onSelectModelChange}
             />
         </Card>
     )
 
-    function CustomToolbar() {
+    function onSelectModelChange(nrows: any) {
+        setSelectedRowsCount(nrows.length)
+    }
+
+    function CustomGridToolbar() {
         return (
-            <div className="custom-toolbar">
-                <Typography className='toolbar-title'>{title}</Typography>
-                <GridToolbar />
-                <Box className='toolbar-selected-summary'>
-                    <Typography color='secondary'>Selected debits:</Typography>
-                    <Typography color='secondary'>Selected credits:</Typography>
-                </Box>
-            </div>
+            <GridToolbarContainer className="custom-toolbar">
+                <Typography variant='h6' className='toolbar-title'>{title}</Typography>
+                <GridToolbarColumnsButton color='secondary' />
+                <GridToolbarFilterButton color='secondary' />
+                <GridToolbarDensitySelector color='secondary' />
+                <GridToolbarExport color='secondary' />
+
+            </GridToolbarContainer>
+            // <div className="custom-toolbar">
+            //     <Typography className='toolbar-title'>{title}</Typography>
+            //     <GridToolbar />
+            //     <Box className='toolbar-selected-summary'>
+            //         <Typography color='secondary'>Selected debits:</Typography>
+            //         <Typography color='secondary'>Selected credits:</Typography>
+            //     </Box>
+            //     <CustomRowCounter />
+            // </div>
         )
+    }
+
+    function CustomGridFooter(props: any) {
+        return (
+            <GridFooterContainer>
+                <span>Selected rows count:{props.selectedRowsCount}</span>
+                <span>Count of rows:{props.count}</span>                
+            </GridFooterContainer>
+        )
+    }
+
+    function CustomRowCounter() {
+        // const { rows } = useGridSlotComponentProps()
+        const rows = []
+        return (<div>Row count: {rows.length}</div>)
     }
 
     async function fetchRows(queryId: string, arg: any) {
@@ -109,16 +145,16 @@ const useStyles: any = makeStyles((theme: Theme) =>
                 alignItems: 'center',
                 '& .toolbar-title': {
                     color: 'dodgerblue',
-                    fontSize:'1.2rem',
+                    fontSize: '1.2rem',
                     fontWeight: 'bold',
                     // lineHeight:'2rem',
                     // height:'1.4rem'
-                    marginTop: theme.spacing(0.4)
+                    // marginTop: theme.spacing(0.4)
                 },
                 '& .toolbar-selected-summary': {
                     display: 'flex',
                     marginTop: theme.spacing(0.4)
-                    
+
                 }
             },
             '& .select': {
