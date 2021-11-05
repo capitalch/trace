bbe819b48d5f84979ad36cbadd6db581d75e51ff
Sushant Agrawal <capitalch@gmail.com>
2021-11-05 13:39:19 +0530

    init

--- lab/adam1/src/components/component6.tsx
+++ lab/adam1/src/components/component6.tsx
@@ -1,10 +1,29 @@
-import React, { useEffect, useState, useRef } from 'react'
-const store: any = {}
+import React, { useEffect, useState, useRef, useContext } from 'react'
+import { Button } from '@mui/material'
+// import { storeInstance } from './mobx-store'
+import { MobxStoreContext } from './mobx-store'
+import { useObserver, observer } from 'mobx-react'
+// const store: any = {}
 function Component6() {
+    // const [, setRefresh] = useState({})
+    const storeInstance: any = useContext(MobxStoreContext)
+    return useObserver(()=>(
+        <div>
+            Component 6<div>Count:{storeInstance.count}</div>
+            <Button
+                variant="contained"
+                onClick={
+                    () => {
+                        storeInstance.incr()
+                        // setRefresh({})
+                    }
 
-    return <div>
-        Component 6
-    </div>
+                    // setRefresh({})
+                }>
+                Increase
+            </Button>
+        </div>
+    ))
 }
 
 export { Component6 }
