3d6efa98e02150d64f1b11a4bd6a79614dc10835
Sushant Agrawal <capitalch@gmail.com>
2021-12-18 14:43:24 +0530

    init

--- lab/adam3/src/components/comp1.tsx
+++ lab/adam3/src/components/comp1.tsx
@@ -1,25 +1,10 @@
-const PDFDocument = require('pdfkit')
-const blobStream  = require('blob-stream')
 
-function Comp1(){
-    const doc = new PDFDocument;
-    doc.text('Hello world!')
-    // pipe the document to a blob
-    const stream = doc.pipe(blobStream());
-
-    doc.end()
-    stream.on('finish', function() {
-        // get a blob you can do whatever you like with
-        const blob = stream.toBlob('application/pdf');
-      
-        // or get a blob URL for display in the browser
-        const url = stream.toBlobURL('application/pdf');
-        // iframe.src = url;
-      })
-
-      return <div>
-          Comp1
-      </div>
+function Comp1() {
+    return (
+        <div>
+            test
+        </div>
+    )
 }
 
-export {Comp1}
+export { Comp1 }
