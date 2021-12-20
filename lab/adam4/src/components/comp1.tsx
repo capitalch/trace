1d510a68711d48799dccca46c02b800a05fa438d
Sushant Agrawal <capitalch@gmail.com>
2021-12-20 21:31:02 +0530

    init

--- lab/adam4/src/components/comp1.tsx
+++ lab/adam4/src/components/comp1.tsx
@@ -2,6 +2,7 @@ import { useRef } from 'react'
 import { useReactToPrint } from 'react-to-print'
 import { Comp1Child } from './comp1-child'
 import styles from './comp1.module.scss'
+import axios from 'axios'
 
 function Comp1() {
     const childRef: any = useRef()
@@ -17,9 +18,33 @@ function Comp1() {
                 <Comp1Child />
             </div>
             <button onClick={handlePrint}>Pdf</button>
+            <button onClick={handlePup}>Puppetter</button>
         </div>
     )
 
+    function handlePup(event:any) {
+        axios.get('http://localhost:5001', {
+            responseType: 'blob',
+            headers: {
+                'Accept': 'application/pdf'
+            }
+        }).then((response) => {
+            const blob:any = new Blob([response.data], { type: 'application/pdf' })
+            const fileURL = URL.createObjectURL(blob)
+            const w:any = window.open(fileURL, "_blank", "height=400,width=600,top=200, left=200")
+            
+            w.print()
+            // w.close()
+            
+            // const link = document.createElement('a')
+            // link.href = window.URL.createObjectURL(blob)
+            // link.download = `your-file-name.pdf`
+            // link.click()
+
+            // event.preventDefault()
+            // window.open(blob, "PRINT", "height=400,width=600")
+        })
+    }
 
 }
 
--- lab/node-lab/index.js
+++ lab/node-lab/index.js
@@ -1,25 +1,77 @@
+const express = require('express')
 const puppeteer = require('puppeteer')
-const http = require('http')
 const fs = require('fs')
+const path = require('path')
 
-const server = http.createServer((req, res) => {
-    res.end('hello')
+const app = express()
+
+app.use(express.static(path.join(__dirname,'public')))
+const allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('cache-control', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Credentials', 'true')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, Accept, Content-Type, Authorization, Content-Length, X-Requested-With, A' +
+            'ccess-Control-Allow-Origin,x-access-token'
+    )
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.sendStatus(200)
+    } else {
+        next()
+    }
+}
+app.use(allowCrossDomain)
+
+const PORT = 5001
+server = app.listen(PORT, function () {
+    console.log(`Listening on port ${server.address().port}`)
 })
 
-async function pup() {
-    const browser = await puppeteer.launch()
+let browser
+(async () => {
+    browser = await puppeteer.launch()
+})()
+
+async function pup () {
+    browser = await puppeteer.launch()
     const page = await browser.newPage()
-    const html = fs.readFileSync(`${__dirname}/template3.html`, 'utf8')
+    // const html = await fs.readFileAsync(`${__dirname}/public/template2.html`, 'utf8')
+    const html = await fs.promises.readFile(path.join(__dirname,'public','template2.html'), 'utf8')
+    
     await page.setContent(html, {
         waitUntil: 'domcontentloaded'
-      })
-    // await page.goto('https://blog.risingstack.com', {
-    //     waitUntil: 'networkidle0',
-    // })
-    const pdf = await page.pdf({ format: 'A4', path:'abc.pdf' })
+    })
+    const pdf = await page.pdf({ format: 'A4', path: 'abc.pdf', printBackground: true })    
     await browser.close()
+    return pdf
 }
-pup()
+// pup()
+
+app.get('/', async (req, res) => {
+    const pdf = await pup()
+    res.set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length })
+	res.send(pdf)
+    // res.end('Hello')
+})
+
+app.get('/invoice', async(req,res)=>{
+    const file = path.join(__dirname,'public', 'template2.html')
+    res.sendFile(file)
+})
+
+app.use((err, req, res, next) => {
+    if (!res.headersSent) {
+        res.status(422).send(err.message)
+    }
+    console.log(err.message)
+    logger.doLog('error', err.message, { place: messages.errApp })
+})
+
+
+
 
 // (async () => {
 //     const browser = await puppeteer.launch();
@@ -29,10 +81,9 @@ pup()
 //     await browser.close();
 // })();
 
-server.listen(3000, async (err) => {
-    console.log('server listening at port 3001')
-})
-
+// server.listen(3000, async (err) => {
+//     console.log('server listening at port 3001')
+// })
 
 // const startDate = Date.now()
 // let endDate
