db9d607729922bae7c3fdcf93a117dd63251325c
Sushant Agrawal <capitalch@gmail.com>
2021-12-27 01:33:35 +0530

    init

--- lab/adam3/src/components/comp2a-hook.tsx
+++ lab/adam3/src/components/comp2a-hook.tsx
@@ -28,7 +28,7 @@ function useComp2() {
                 paddingTop: 10,
                 paddingLeft: 20,
                 paddingRight: 20,
-                paddingBottom: 100,
+                paddingBottom: 20,
                 fontFamily: 'Helvetica',
             },
             pageNumber: {
@@ -115,7 +115,7 @@ function useComp2() {
                         fontWeight: 'bold',
                     },
                     gstin: {
-                        marginTop: 5,
+                        marginTop: 3,
                         fontSize: 12,
                         fontWeight: 'bold',
                     },
@@ -161,6 +161,7 @@ function useComp2() {
                         fontWeight: 'bold',
                         fontSize: 14,
                         marginTop: 5,
+                        textTransform: 'uppercase',
                     },
                     buyersCopy: {
                         fontSize: 10,
@@ -181,12 +182,15 @@ function useComp2() {
                 const ii = invoiceData.invoiceInfo
                 return (
                     <View style={styles.headerInvoice}>
-                        <Text style={styles.taxInvoice}>
-                            {invoiceData.invoiceInfo.title}
-                        </Text>
-                        <Text style={styles.buyersCopy}>
-                            {invoiceData.invoiceInfo.titleRight}
-                        </Text>
+                        <View style={{ flexDirection: 'row', justifyContent:'space-between', alignItems:'center' }}>
+                            <Text style={styles.taxInvoice}>
+                                {invoiceData.invoiceInfo.title}
+                            </Text>
+                            <Text style={styles.buyersCopy}>
+                                {invoiceData.invoiceInfo.titleRight}
+                            </Text>
+                        </View>
+
                         <Text style={styles.invoiceNo}>
                             Invoice #: {ii.invoiceNo}
                         </Text>
@@ -196,9 +200,9 @@ function useComp2() {
                         <Text style={gStyles.normal}>
                             {''.concat('Type: ', ii.type)}
                         </Text>
-                        <Text style={gStyles.normal}>
+                        {/* <Text style={gStyles.normal}>
                             {''.concat('Terms: ', ii.terms)}
-                        </Text>
+                        </Text> */}
                     </View>
                 )
             }
@@ -357,8 +361,8 @@ function useComp2() {
                         <View
                             style={{
                                 flexDirection: 'row',
-                                paddingTop: 5,
-                                paddingBottom: 5,
+                                paddingTop: 3,
+                                // paddingBottom: 5,
                             }}
                             key={keyGen()}>
                             <Text style={[gStyles.normal, { width: 20 }]}>
@@ -440,7 +444,7 @@ function useComp2() {
                     },
                 })
                 return (
-                    <View style={styles.summary} debug>
+                    <View style={styles.summary}>
                         <View
                             style={{
                                 fontWeight: 'bold',
@@ -518,9 +522,7 @@ function useComp2() {
                         )
                     })
                     return (
-                        <View style={{ flexDirection: 'column' }} debug>
-                            {rows}
-                        </View>
+                        <View style={{ flexDirection: 'column' }}>{rows}</View>
                     )
                     function keyGen() {
                         return ++counter
@@ -551,8 +553,7 @@ function useComp2() {
                         style={{
                             flexDirection: 'column',
                             justifyContent: 'flex-end',
-                        }}
-                        debug>
+                        }}>
                         <Text></Text>
                         <Text></Text>
                         <Text style={styles.signatoryItem}>
@@ -577,33 +578,33 @@ function useComp2() {
             )
         }
 
-        // function Footer() {
-        //     return (
-        //         <View style={[gStyles.footer]} >
-        //             <Text style={{ fontSize: 10 }}>
-        //                 One lac twenty thousand only
-        //             </Text>
-        //             <Text
-        //                 style={{
-        //                     fontSize: 12,
-        //                     fontWeight: 'bold',
-        //                     textDecoration: 'underline',
-        //                 }}>
-        //                 Amount payable: 1200000
-        //             </Text>
-        //         </View>
-        //     )
-        // }
-
         function Footer() {
             return (
-                <View style={[gStyles.footer]} 
-                render = {()=><Text>Test</Text>} fixed
-                >
-                    
+                <View style={[gStyles.footer]}>
+                    <Text style={{ fontSize: 12 }}>
+                        One lac twenty thousand only
+                    </Text>
+                    <Text
+                        style={{
+                            fontSize: 12,
+                            fontWeight: 'bold',
+                            textDecoration: 'underline',
+                        }}>
+                        Amount payable: 1200000
+                    </Text>
                 </View>
             )
         }
+
+        // function Footer() {
+        //     return (
+        //         <View style={[gStyles.footer]}
+        //         render = {()=><Text>Test</Text>} fixed
+        //         >
+
+        //         </View>
+        //     )
+        // }
 
         function PageNo() {
             return (
@@ -620,18 +621,3 @@ function useComp2() {
     return { InvoicePdf }
 }
 export { useComp2 }
-
-// function HorLine() {
-//     return (
-//         <Svg height={15} fixed>
-//             <Line
-//                 x1="0"
-//                 y1="10"
-//                 x2='590'
-//                 y2="10"
-//                 strokeWidth={0.5}
-//                 stroke="rgb(0,0,0)"
-//             />
-//         </Svg>
-//     )
-// }
