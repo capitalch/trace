28a1f258c8f162dbc7d0e755d614c17915d36c0d
Sushant Agrawal <capitalch@gmail.com>
2022-02-23 21:37:12 +0530

    init'

---
+++ dev/trace-client-next/src/entities/accounts/components/inventory/products-op-bal-hook.tsx
@@ -0,0 +1,26 @@
+import {
+    moment,
+    useState,
+    useEffect,
+    useRef,
+} from '../../../../imports/regular-imports'
+import { useSharedElements } from '../common/shared-elements-hook'
+import {DataGridPro, useGridApiRef,} from '../../../../imports/gui-imports'
+
+function useProductsOpBal(){
+    const meta = useRef({
+
+    })
+    const {} = useSharedElements()
+    useEffect(()=>{
+
+        return(()=>{
+
+        })
+        
+    },[])
+
+
+}
+
+export {useProductsOpBal}
