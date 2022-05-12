eb22826260874bb35be2ccd6417f57298d1e5c78
Sushant Agrawal <sagarwal@netwoven.com>
2022-05-12 12:50:15 +0530

    init

--- lab/react-native/adam1/App.tsx
+++ lab/react-native/adam1/App.tsx
@@ -12,11 +12,13 @@ import React from 'react';
 import {
   ActivityIndicator,
   Button,
+  FlatList,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TouchableHighlight,
   useColorScheme,
   View,
 } from 'react-native';
@@ -30,104 +32,55 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 function App() {
-  return (<View style={[styles.container]}>
-    <Text style={{fontSize:16, color:'red'}}>Test</Text>
-    <ActivityIndicator animating={false} size='large' style={{marginLeft:10}} />
-  </View>)
+
+  function renderItem({ item, index, separators }: any) {
+    return (
+      <TouchableHighlight
+        key={item.id}>
+        <Text style={styles.listItem} >{item.name}</Text>
+      </TouchableHighlight>
+    )
+  }
+
+  function SeperatorComponent() {
+    return (
+      <View
+        style={{
+          height: 2,
+          width: "100%",
+          backgroundColor: "blue",
+        }}
+      />)
+  }
+
+
+  return (<SafeAreaView style={[styles.container]}>
+    <FlatList
+      data={data}
+      renderItem={renderItem}
+      ItemSeparatorComponent={SeperatorComponent}
+    ></FlatList>
+  </SafeAreaView>)
 }
+
+
 const styles = StyleSheet.create({
   container: {
     padding: 10,
-    flexDirection:'row',
-    alignItems:'center',
-    // justifyContent:'space-around'
-    // color: Colors.red,
+    // flexDirection:'row',
+    // alignItems:'center',
+  },
+  listItem: {
+    padding:10,
+    // borderWidth:1,
+    // borderColor:'grey'
   }
 })
-
-// const Section: React.FC<{
-//   title: string;
-// }> = ({children, title}) => {
-//   const isDarkMode = useColorScheme() === 'dark';
-//   return (
-//     <View style={styles.sectionContainer}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? Colors.white : Colors.black,
-//           },
-//         ]}>
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? Colors.light : Colors.dark,
-//           },
-//         ]}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// };
-
-// const App = () => {
-//   const isDarkMode = useColorScheme() === 'dark';
-
-//   const backgroundStyle = {
-//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-//   };
-
-//   return (
-//     <SafeAreaView style={backgroundStyle}>
-//       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-//       <ScrollView
-//         contentInsetAdjustmentBehavior="automatic"
-//         style={backgroundStyle}>
-//         <Header />
-//         <View
-//           style={{
-//             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-//           }}>
-//           <Section title="Step One">
-//             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-//             screen and then come back to see your edits.
-//           </Section>
-//           <Section title="See Your Changes">
-//             <ReloadInstructions />
-//           </Section>
-//           <Section title="Debug">
-//             <DebugInstructions />
-//           </Section>
-//           <Section title="Learn More">
-//             Read the docs to discover what to do next:
-//           </Section>
-//           <LearnMoreLinks />
-//         </View>
-//       </ScrollView>
-//     </SafeAreaView>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-// });
-
 export default App;
+
+const data = [
+  { id: 1, name: 'aaa' },
+  { id: 2, name: 'bbb' },
+  { id: 3, name: 'ccc' },
+  { id: 4, name: 'ddd' },
+]
