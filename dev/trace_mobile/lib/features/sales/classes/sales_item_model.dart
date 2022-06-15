360fb6a20152a52e15dc72714fd6ba0c74e7e21c
Sushant Agrawal <sagarwal@netwoven.com>
2022-06-15 11:23:57 +0530

    init

--- dev/trace_mobile/lib/features/sales/classes/sales_item_model.dart
+++ dev/trace_mobile/lib/features/sales/classes/sales_item_model.dart
@@ -1,19 +1,24 @@
 class SalesItemModel {
   SalesItemModel({
-    required age,
-    required brandName,
-    required catName,
-    required stock,
-    required info,
-    required label,
-    required qty,
-    required aggrSale,
-    required cgst,
-    required sgst,
-    required igst,
-    required amount,
-    required grossProfit,
-    required lastPurchaseDate,
+    required this.age,
+    required this.brandName,
+    required this.catName,
+    required this.stock,
+    required this.info,
+    required this.label,
+    required this.qty,
+    required this.aggrSale,
+    required this.cgst,
+    required this.sgst,
+    required this.igst,
+    required this.amount,
+    required this.grossProfit,
+    required this.lastPurchaseDate,
+    required this.price,
+    required this.saleType,
+    required this.accounts,
+    required this.autoRefNo,
+    required this.gstRate,
     // required this.mrp,
     // required this.pur,
     // required this.purGst,
@@ -23,12 +28,26 @@ class SalesItemModel {
 
   factory SalesItemModel.fromJson({required Map<String, dynamic> j}) {
     return SalesItemModel(
-        age: (j['age'] ?? 0).toString(),
-        brandName: j['brandName'],
-        catName: j['catName'],
-        stock: double.parse(j['stock'].toString()),
-        info: j['info'],
-        label: j['label']);
+      age: double.parse((j['age'] ?? 0).toString()),
+      brandName: j['brandName'],
+      catName: j['catName'],
+      stock: double.parse(j['stock'].toString()),
+      info: j['info'],
+      label: j['label'],
+      qty: double.parse(j['qty'].toString()),
+      aggrSale: double.parse(j['aggrSale'].toString()),
+      cgst: double.parse(j['cgst'].toString()),
+      sgst: double.parse(j['sgst'].toString()),
+      igst: double.parse(j['igst'].toString()),
+      amount: double.parse(j['amount'].toString()),
+      grossProfit: double.parse(j['grossProfit'].toString()),
+      lastPurchaseDate: j['lastPurchaseDate'],
+      price: double.parse(j['price'].toString()),
+      saleType: j['saleType'],
+      accounts: j['accounts'],
+      autoRefNo: j['autoRefNo'],
+      gstRate: double.parse(j['gstRate'].toString()),
+    );
   }
 
   final double age;
@@ -46,10 +65,9 @@ class SalesItemModel {
   final double amount;
   final double grossProfit;
   final String lastPurchaseDate;
-
-  // final double mrp;
-  // final double pur;
-  // final double purGst;
-  // final double salGst;
-  // final double ytd;
+  final double price;
+  final String saleType;
+  final String accounts;
+  final String autoRefNo;
+  final double gstRate;
 }
--- dev/trace_mobile/lib/features/sales/sales_page.dart
+++ dev/trace_mobile/lib/features/sales/sales_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:trace_mobile/common/classes/global_settings.dart';
 import 'package:trace_mobile/common/classes/graphql_queries.dart';
 import 'package:trace_mobile/common/classes/utils.dart';
+import 'package:trace_mobile/features/sales/classes/sales_item_model.dart';
 import 'package:trace_mobile/features/sales/classes/sales_query_props.dart';
 import 'package:trace_mobile/features/sales/classes/sales_state.dart';
 
@@ -49,7 +51,7 @@ class SalesReportHeader extends StatelessWidget {
             alignment: Alignment.center,
             child: Text(
               'Sale ${props.labelName} (${Utils.toLocalDateString(props.startDate)} to ${Utils.toLocalDateString(props.endDate)})',
-              style: Theme.of(context).textTheme.subtitle2,
+              style: Theme.of(context).textTheme.subtitle2?.copyWith(fontWeight: FontWeight.bold),
             )));
       },
     );
@@ -78,12 +80,12 @@ class SalesReportBody extends StatelessWidget {
             args: {
               'startDate': Utils.toIsoDateString(props.startDate),
               'endDate': Utils.toIsoDateString(props.endDate),
-              'tagId': '1'
+              'tagId': '0'
             });
         return FutureBuilder(
           future: salesFuture,
           builder: (context, snapshot) {
-            var widget = const Text('');
+            dynamic widget = const Text('');
             if (snapshot.connectionState == ConnectionState.waiting) {
               widget = Text('Loading...',
                   style: Theme.of(context).textTheme.headline6);
@@ -94,14 +96,14 @@ class SalesReportBody extends StatelessWidget {
                 List<dynamic> dataList =
                     snapshot.data?.data?['accounts']?['genericView'] ?? [];
                 if (dataList.isEmpty) {
-                  return Center(
+                  widget = Center(
                     child: Text(
                       'No data',
                       style: Theme.of(context).textTheme.headline6,
                     ),
                   );
                 } else {
-                  return ListViewSalesData(
+                  widget = ListViewSalesData(
                     dataList: dataList,
                   );
                 }
@@ -116,40 +118,170 @@ class SalesReportBody extends StatelessWidget {
             ));
           },
         );
-
-        // Expanded(
-        //     child: ListView.builder(
-        //   itemCount: 1,
-        //   itemBuilder: (context, index) {
-        //     return Text('Test');
-        //   },
-        // ));
       },
     );
   }
 }
 
 class ListViewSalesData extends StatelessWidget {
-  ListViewSalesData({Key? key, required dataList}) : super(key: key);
+  const ListViewSalesData({Key? key, required this.dataList}) : super(key: key);
 
-  final List<dynamic> dataList = [];
+  final List<dynamic> dataList;
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: dataList.length,
-      itemBuilder: (context, index) {},
+      itemBuilder: (context, index) {
+        return SalesCardItem(
+            index: index + 1,
+            indexedItem: SalesItemModel.fromJson(j: dataList[index]));
+      },
     );
   }
 }
 
 class SalesCardItem extends StatelessWidget {
-  const SalesCardItem({Key? key}) : super(key: key);
+  const SalesCardItem(
+      {Key? key, required this.index, required this.indexedItem})
+      : super(key: key);
+
+  final int index;
+  final SalesItemModel indexedItem;
 
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-    
-    return Container();
+    double stock = indexedItem.stock;
+    var title = [
+      indexedItem.brandName,
+      ' ',
+      indexedItem.catName,
+      ' ',
+      indexedItem.label
+    ].join();
+    // var subTitle1 = indexedItem.info;
+    var subTitle = [
+      indexedItem.info,
+      ' ',
+      'Sale type: ',
+      indexedItem.saleType,
+      ' Accounts: ', indexedItem.accounts
+    ].join();
+    var formatter = NumberFormat('#,##,000');
+    return Center(
+        child: Card(
+            elevation: 1,
+            color: ((indexedItem.age) >= 360)
+                ? Colors.pink.shade100
+                : Colors.grey.shade100,
+            child: Column(children: [
+              Container(
+                padding: const EdgeInsets.only(top: 15),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(left: 70),
+                        child: Text(
+                          'AGE: ${indexedItem.age.toStringAsFixed(0)}',
+                          style: const TextStyle(fontWeight: FontWeight.w900),
+                        ),
+                      ),
+                      Text(indexedItem.autoRefNo),
+                      Container(
+                        padding: const EdgeInsets.only(right: 15),
+                        child: Text(
+                            'GP: ${indexedItem.grossProfit.toStringAsFixed(0)}',
+                            style:
+                                const TextStyle(fontWeight: FontWeight.w900)),
+                      )
+                    ]),
+              ),
+              ListTile(
+                  leading: Text(
+                    index.toString(),
+                    style: theme.textTheme.subtitle1
+                        ?.copyWith(color: Colors.brown),
+                  ),
+                  title: Text(
+                    title,
+                    style: theme.textTheme.bodyLarge?.copyWith(
+                      color: Colors.blue.shade700,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                  subtitle: Text(
+                    subTitle,
+                    style: theme.textTheme.bodyText1,
+                  ),
+                  dense: true,
+                  trailing: CircleAvatar(
+                      radius: 20,
+                      backgroundColor: (indexedItem.stock == 0)
+                          ? Colors.white
+                          : Colors.grey.shade800,
+                      child: Text(
+                        stock.toStringAsFixed(0),
+                        style: theme.textTheme.subtitle2?.copyWith(
+                            color: (indexedItem.stock == 0)
+                                ? Colors.black
+                                : Colors.white),
+                      ))),
+              const SizedBox(
+                height: 5,
+              ),
+              Container(
+                padding: const EdgeInsets.only(bottom: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.only(left: 70),
+                      child: Text(
+                        'Price: ${formatter.format(indexedItem.price * (1 + indexedItem.gstRate/100))}',
+                        style: const TextStyle(fontWeight: FontWeight.w900),
+                      ),
+                    ),
+                    Text(
+                      'Qty: ${formatter.format(indexedItem.qty)}',
+                      // style: const TextStyle(fontWeight: FontWeight.w900),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(right: 15),
+                      child: Text(
+                        'Amount: ${formatter.format(indexedItem.amount)}',
+                        style: const TextStyle(fontWeight: FontWeight.w900),
+                      ),
+                    )
+                  ],
+                ),
+              )
+
+              // Container(
+              //   padding: const EdgeInsets.only(bottom: 15),
+              //   child: Row(
+              //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //       crossAxisAlignment: CrossAxisAlignment.center,
+              //       children: [
+              //         Container(
+              //           padding: const EdgeInsets.only(left: 70),
+              //           child: Text(
+              //             'PUR(GST): ${formatter.format(indexedItem.purGst)}',
+              //             style: const TextStyle(fontWeight: FontWeight.w900),
+              //           ),
+              //         ),
+              //         Container(
+              //           padding: const EdgeInsets.only(right: 15),
+              //           child: Text(
+              //             'SAL(GST): ${formatter.format(indexedItem.salGst)}',
+              //             style: const TextStyle(fontWeight: FontWeight.w900),
+              //           ),
+              //         )
+              //       ]),
+              // ),
+            ])));
   }
 }
 
