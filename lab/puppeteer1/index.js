c7e4a3fde681f1559c79b7dbb3c3fe64dcd3e9a6
Sushant Agrawal <sagarwal@netwoven.com>
2022-07-30 13:21:43 +0530

    init

--- lab/puppeteer1/index.js
+++ lab/puppeteer1/index.js
@@ -1,13 +1,39 @@
-const puppeteer = require('puppeteer');
-// const fs = require('fs')
+const puppeteer = require('puppeteer')
+const express = require('express')
+const fs = require('fs')
 
-(async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://netwoven.com', {
-      waitUntil: 'networkidle2',
-    });
-    await page.pdf({path: 'hn.pdf', format: 'a4'});
-  
-    await browser.close();
-  })();
+const app = express()
+//(async () => {
+// const browser = await puppeteer.launch()
+// const page = await browser.newPage()
+//     await page.goto('https://netwoven.com', {
+//         waitUntil: 'networkidle2',
+//     })
+//     await page.pdf({ path: 'hn.pdf', format: 'a4' })
+
+//     await browser.close()
+// })()
+
+app.get('/', (req, res) => {
+    res.send('hello')
+})
+
+app.get('/pdf', async (req, res) => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    const fileBuffer = fs.readFile('./x.pdf', (err, data) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.end(data)
+        }
+    })
+    browser.close()
+    page.close()
+})
+
+const server = app.listen(8081, () => {
+    const port = server.address().port
+    const host = server.address().address
+    console.log(`Server lstening at host: ${host}, port:${port}`)
+})
